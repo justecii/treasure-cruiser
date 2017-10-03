32460bb79fbbce2b5411a075dad7a3212dfa3dcd
justecii <justecii@gmail.com>
2017-10-03 10:14:05 -0700

    search works with api. need to format results

--- controllers/search.js
+++ controllers/search.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+var bodyParser = require('body-parser');
 var passport = require('../config/ppConfig');
 var request = require('request');
 
 router.get("/", function(req, res) {
-    var scryApi = "https://api.scryfall.com/cards/search?q=tarmogoyf";
-    request(scryApi, function(error, response, body) {
-        if (error) {
-            return res.send(error)
-        }
-        var result = JSON.parse(body).results;
-        console.log(result);
-    });
-    // var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151';
-    // request(pokemonUrl, function(error, response, body) {
-    //     if (error) {
-    //         return res.send(error);
-    //     }
+    res.render('search/index')
+});
 
-    //     var result = JSON.parse(body).results;
-    //     console.log(result);
-    //     // res.render('index', { pokemon: result });
-    // });
+router.post("/results", function(req, res) {
+    var scryApi = "https://api.scryfall.com/cards/search?q=";
+    var q = req.body.name;
+    request(scryApi + q, function(error, response, body) {
+        if (error) {
+            return res.send(error);
+        }
+        var data = JSON.parse(body);
+        res.send(data);
+    });
 });
 
 module.exports = router;
