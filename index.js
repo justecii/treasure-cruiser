514f91085a284bd9fe7b6a5e7a33c2690cbd668c
justecii <justecii@gmail.com>
2017-10-05 14:19:45 -0700

    heroku time

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ app.use('/auth', require('./controllers/auth'));
 app.use('/search', require('./controllers/search'));
 app.use('/collection', require('./controllers/collection'))
 
+//heroku will look for the process.env.port and deploy to an open space
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
